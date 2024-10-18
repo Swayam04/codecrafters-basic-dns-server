3e90752b4c2ab56399cd8ec2e6dacc03ae3df94d
Swayamjeet <swayam.hp001@gmail.com>
2024-10-18 16:28:47 +0530

    Converted to a Forwarding Server if Address is Provided

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,14 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-      DNSServerSocket serverSocket = new DNSServerSocket(2053);
+
+      String forwardingAddress = "";
+      for(int i = 0; i < args.length; i++){
+        if(args[i].equals("--resolver")) {
+          forwardingAddress = args[i + 1];
+        }
+      }
+      DNSServerSocket serverSocket = new DNSServerSocket(2053, forwardingAddress);
       serverSocket.serve();
   }
 }
--- src/main/java/udp/DNSServerSocket.java
+++ src/main/java/udp/DNSServerSocket.java
@@ -9,13 +9,16 @@ import utils.DNSMessageParser;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class DNSServerSocket {
 
     private final int PORT;
+    private final String forwardingAddress;
 
-    public DNSServerSocket(int port) {
+    public DNSServerSocket(int port, String forwardingAddress) {
         PORT = port;
+        this.forwardingAddress = forwardingAddress;
     }
 
     public void serve() {
@@ -42,7 +45,7 @@ public class DNSServerSocket {
         }
     }
 
-    private static DNSHeader getResponseHeader(DNSMessage request) {
+    private DNSHeader getResponseHeader(DNSMessage request) {
         DNSHeader requestHeader = request.getHeader();
 
         DNSHeader responseHeader = new DNSHeader();
@@ -56,10 +59,54 @@ public class DNSServerSocket {
         return responseHeader;
     }
 
-    private static void getQuestionsAndAnswers(DNSMessage request, DNSMessage response) {
+    private void getQuestionsAndAnswers(DNSMessage request, DNSMessage response) {
         for(DNSQuestion question : request.getQuestions()) {
             response.addQuestion(question);
-            response.addAnswer(new DNSRecord(question.getDomainName(), DNSType.A, DNSClass.IN, 256, 4, "8.8.8.8"));
+            if(forwardingAddress != null && !forwardingAddress.isEmpty()) {
+                response.addAnswer(forwardRequestAndGetResponse(question, request.getHeader().getID()));
+            } else {
+                response.addAnswer(new DNSRecord(question.getDomainName(), DNSType.A, DNSClass.IN, 256, 4, "8.8.8.8"));
+            }
         }
+    }
+
+    private DNSRecord forwardRequestAndGetResponse(DNSQuestion question, short id) {
+        DNSHeader header = new DNSHeader();
+        header.setID(id);
+        header.setQR(false);
+        header.setRD(true);
+        header.setQuestionCount((short) 1);
+
+        DNSMessage singleRequest = new DNSMessage(header);
+        singleRequest.addQuestion(question);
+
+        byte[] request = singleRequest.getMessage();
+
+        String[] addressParts = forwardingAddress.split(":");
+        String host = addressParts[0];
+        int port = Integer.parseInt(addressParts[1]);
+
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.setSoTimeout(50000);
+
+            InetAddress address = InetAddress.getByName(host);
+            DatagramPacket requestPacket = new DatagramPacket(request, request.length, address, port);
+            socket.send(requestPacket);
+
+            byte[] responseBuffer = new byte[512];
+            DatagramPacket responsePacket = new DatagramPacket(responseBuffer, responseBuffer.length);
+            socket.receive(responsePacket);
+            DNSMessageParser parser = new DNSMessageParser(responseBuffer);
+            DNSMessage response = parser.parseMessage();
+
+            if(response.getHeader().getAnswerCount() != 1) {
+                throw new Exception("Incorrect answer count");
+            }
+
+            return response.getAnswers().getFirst();
+        } catch (Exception e) {
+            System.out.println("Failed to send request: " + e.getMessage());
+        }
+        return null;
     }
 }
